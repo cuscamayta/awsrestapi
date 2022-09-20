52f1b66b792cdd5e3a4c348cba534f08941923b4
Carlos Uscamayta <uscamayta.carlos@gmail.com>
2022-09-20 11:25:38 -0400

    simple rest api

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express")
+var app = express()
+// var db = require("./database.js")
+// var md5 = require("md5")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.get("/api/users", (req, res, next) => {
+    res.json({"message":"Ok from get"});
+});
+
+app.get("/api/", (req, res, next) => {
+    res.json({"message":"Ok from home"});
+});
+
+app.get("/api/user/:id", (req, res, next) => {
+    res.json({message:'this is a get users'});
+});
